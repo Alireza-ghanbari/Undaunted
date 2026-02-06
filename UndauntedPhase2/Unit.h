27035c0f0e95bcce94c92243a82ad0bb610ee7a2
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2026-02-07 01:07:20 +0330

    phase 2: fix unit model

--- UndauntedPhase2/Unit.h
+++ UndauntedPhase2/Unit.h
@@ -1,10 +1,35 @@
 #ifndef UNIT_H
 #define UNIT_H
 
+#include <QString>
+
+class Cell;
+class Player;
+
+enum class UnitType {
+    Scout,
+    Sniper,
+    Sergeant
+};
+
 class Unit
 {
 public:
-    Unit();
+    Unit(UnitType type, Player *owner);
+
+    UnitType type() const;
+    int hp() const;
+
+    void setCell(Cell *cell);
+    Cell* cell() const;
+
+    Player* owner() const;
+
+private:
+    UnitType m_type;
+    int m_hp;
+    Cell *m_cell = nullptr;
+    Player *m_owner = nullptr;
 };
 
-#endif // UNIT_H
+#endif
