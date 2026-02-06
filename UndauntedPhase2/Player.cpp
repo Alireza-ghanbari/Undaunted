75e1fcdecc27f296c8ec404307e494da317f457d
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2026-02-07 01:23:43 +0330

    phase 2: fix player model

--- UndauntedPhase2/Player.cpp
+++ UndauntedPhase2/Player.cpp
@@ -1,3 +1,18 @@
 #include "Player.h"
+#include "Unit.h"
 
-Player::Player() {}
+Player::Player(const QString &name)
+    : m_name(name)
+{}
+
+QString Player::name() const {
+    return m_name;
+}
+
+void Player::addUnit(Unit *unit) {
+    m_units.append(unit);
+}
+
+const QVector<Unit*>& Player::units() const {
+    return m_units;
+}
--- UndauntedPhase2/Player.h
+++ UndauntedPhase2/Player.h
@@ -1,10 +1,24 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <QString>
+#include <QVector>
+
+class Unit;
+
 class Player
 {
 public:
-    Player();
+    Player(const QString &name);
+
+    QString name() const;
+
+    void addUnit(Unit *unit);
+    const QVector<Unit*>& units() const;
+
+private:
+    QString m_name;
+    QVector<Unit*> m_units;
 };
 
-#endif // PLAYER_H
+#endif
