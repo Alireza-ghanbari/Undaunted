6d6343bae0d3c3c43ac3bd5d4dc6d1ebc844f87e
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2026-02-07 01:50:48 +0330

    phase 2: update cell model

--- UndauntedPhase2/Cell.cpp
+++ UndauntedPhase2/Cell.cpp
@@ -1,8 +1,32 @@
 #include "Cell.h"
+#include "Unit.h"
 
-Cell::Cell() : m_type(0) {}
+Cell::Cell() {}
 
 Cell::Cell(const QString &id, int type)
     : m_id(id), m_type(type)
+{}
+
+QString Cell::id() const { return m_id; }
+int Cell::type() const { return m_type; }
+
+void Cell::setUnit(Unit *unit)
 {
+    m_unit = unit;
+}
+
+Unit* Cell::unit() const
+{
+    return m_unit;
+}
+
+void Cell::addNeighbor(Cell *cell)
+{
+    if (cell && !m_neighbors.contains(cell))
+        m_neighbors.append(cell);
+}
+
+const QList<Cell*>& Cell::neighbors() const
+{
+    return m_neighbors;
 }
--- UndauntedPhase2/Cell.h
+++ UndauntedPhase2/Cell.h
@@ -2,6 +2,9 @@
 #define CELL_H
 
 #include <QString>
+#include <QList>
+
+class Unit;
 
 class Cell
 {
@@ -9,12 +12,22 @@ public:
     Cell();
     Cell(const QString &id, int type);
 
-    QString id() const { return m_id; }
-    int type() const { return m_type; }
+    QString id() const;
+    int type() const;
+
+    // ---- فاز ۲ ----
+    void setUnit(Unit *unit);
+    Unit* unit() const;
+
+    void addNeighbor(Cell *cell);
+    const QList<Cell*>& neighbors() const;
 
 private:
     QString m_id;
     int m_type;
+
+    Unit *m_unit = nullptr;
+    QList<Cell*> m_neighbors;
 };
 
 #endif
